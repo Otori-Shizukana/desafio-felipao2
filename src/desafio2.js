199eebd6d11ac107795b38cc2620b5d91652e8e7
Otori-Shizukana <tdctacilo7@gmail.com>
2024-07-05 12:11:50 -0300

    segundo desafio do felipao

---
+++ src/desafio2.js
@@ -0,0 +1,37 @@
+let vitorias = 0
+let derrotas = 0
+
+
+function analisandoRanque(vitorias, derrotas){
+    let calculo = vitorias - derrotas
+    return calculo
+}
+let saldoVitorias = analisandoRanque(vitorias, derrotas)
+function patentes(patente){
+    while(patente <= 0){
+        console.log(`Jogador muito ruim tem saldo de ${patente}, não pode ser classificado em nenhuma rank, por favor melhore suas habilidades para ser classificado.`)
+        return
+    }
+    let classificao = ''
+    if (patente >= 1 && patente <= 10){
+        classificao = 'Ferro'
+    } else if(patente >= 11 && patente <= 20){
+        classificao = 'Bronze'
+    } else if(patente >= 21 && patente <= 50 ){
+        classificao = 'Prata'
+    } else if(patente >= 51 && patente <= 80){
+        classifi = 'Ouro'
+    } else if(patente >= 81 && patente <= 90){
+        classificao = 'Diamante'
+    } else if(patente >= 91 && patente <= 100){
+        classificao = 'Lendário'
+    } else if(patente >= 101){
+        classificao = 'Imortal'
+    }
+    console.log(`O Herói tem de saldo de ${saldoVitorias}, está nível de ${classificao}`)
+}
+let saldoPatentes = patentes(saldoVitorias)
+
+
+
+
